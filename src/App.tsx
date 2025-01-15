5238838cf1aee3f5d52bdf0186e326caf29ded1e
saraswathy <saraswathychandra88@gmail.com>
2025-01-15 10:08:04 +0100

    UI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,30 @@ function App() {
 					<img src={dpsLogo} className="logo" alt="DPS logo" />
 				</a>
 			</div>
+			
 			<div className="home-card">
-				<p>Your solution goes here 😊</p>
+				{/* Input */}
+				<div className='inner-card'>
+					<div>	
+						<label htmlFor='name'>Name <input id='name' type='text' placeholder='search by name'/></label>
+					</div>
+					
+					{/* city */}
+					<div>
+				
+						<label htmlFor='city'>City <select id='city'><option value=''>--choose City--</option>
+						</select></label>
+					</div>
+					{/* checkbox */}
+					<div>
+						<label htmlFor='oldest city'>Highlight oldest per city <input id='oldestCity' type='checkbox' /></label>
+					</div>
+					
+				</div>
+				<table><thead><tr><th>Name</th><th>City</th><th>Birthday</th></tr></thead></table>
 			</div>
+			
+			
 		</>
 	);
 }
---
+++ src/component/Search.tsx
@@ -0,0 +1,2 @@
+const Search=()=>{return(<div></div>)}
+export default Search;
